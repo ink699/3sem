492ae825766d208f545f708c119780791a69701e
Ilya Kochergin <ink699@mail.ru>
2017-11-13 04:47:59 +0300

    Task 4 was added

---
+++ task_4/recieve.cpp
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define last 255
+#define send 1
+#define recieve 0
+
+int main()
+{
+  char* c;
+  char path[] = "send.cpp";
+  int id = 0, len = 0;
+  key_t key;
+  string filename;
+  cin >> filename;
+  ofstream out(filename);
+  struct mymsgbuf
+  {
+    long type;
+    char text [1000];
+  } mybuf;
+  if((key = ftok(path, 0)) < 0)
+  {
+    exit(-1);
+  }
+  if((id = msgget(key, 0666 | IPC_CREAT)) < 0)
+  {
+    exit (-1);
+  }
+  for(;;)
+  {
+    if((len = msgrcv(id, &mybuf, 1000, 0, 0)) < 0)
+    {
+      msgctl(id, IPC_RMID, NULL);
+      exit(-1);
+    }
+    cout << len << endl;
+    if(mybuf.type == last)
+    {
+      out << endl;
+      msgctl(id, IPC_RMID, NULL);
+      return 0;
+    }
+    for(int i = 0; i < len; ++i)
+    {
+      out << mybuf.text[i];
+    }
+  }
+  msgctl(id, IPC_RMID, NULL);;
+  exit(-1);
+}
---
+++ task_4/send.cpp
@@ -0,0 +1,72 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define last 255
+#define send 1
+#define recieve 0
+
+int main()
+{
+  char c;
+  char path[] = "send.cpp";
+  int id = 0 , len = 0, counter = 0;
+  key_t key;
+  string filename;
+  cin >> filename;
+  ifstream in(filename);
+  struct mymsgbuf
+  {
+    long type;
+    char text [1000];
+  } mybuf;
+  if((key = ftok(path, 0)) < 0)
+  {
+    exit(-1);
+  }
+  if((id = msgget(key, 0666 | IPC_CREAT)) < 0)
+  {
+    exit (-1);
+  }
+  while(in.get(c))
+  {
+    mybuf.type = send;
+    (mybuf.text)[counter] = c;
+    ++counter;
+    if(counter == 1000)
+    {
+      if (msgsnd(id, &mybuf, 1000, 0) < 0)
+      {
+        cout << "probsnd";
+        msgctl(id, IPC_RMID, NULL);
+        exit(-1);
+      }
+      counter = 0;
+    }
+  }
+  if(counter != 0)
+  {
+    if (msgsnd(id, &mybuf, counter, 0) < 0)
+    {
+      cout << "probsnd";
+      msgctl(id, IPC_RMID, NULL);
+      exit(-1);
+    }
+  }
+  mybuf.type = last;
+  (mybuf.text)[0] = '0';
+  if (msgsnd(id, &mybuf, 1, 0) < 0)
+  {
+    cout << "probsnd";
+    msgctl(id, IPC_RMID, NULL);
+    exit(-1);
+  }
+  return 0;
+}
