06267f168fda9d0efff0a3ba1db441131025c64b
Ilya Kochergin <ink699@mail.ru>
2017-12-16 07:03:54 +0300

    Task_5 was added

--- task_5/task5.cpp
+++ task_5/task5.cpp
@@ -11,6 +11,7 @@
 #include <sys/mman.h>
 #include <vector>
 #include <fcntl.h>
+#include <ctime>
 using namespace std;
 
 vector <int> cr_files()
@@ -34,9 +35,13 @@ vector <int> cr_files()
 int main(int argc, const char* argv[])
 {
   vector <int> exp = cr_files();
-  int res_ds = open("res", O_RDWR | O_CREAT, 0666);
+  char buf[4096];
+  int len = 0;
+  int res_ds = open("res", O_RDWR | O_CREAT | O_APPEND, 0666);
   ofstream res_out("res");
+  ofstream log_out("log");
   char *start, *beg_res;
+  double tm;
   for(int i = 10; i <= 25; ++i)
   {
     ftruncate(res_ds, 0);
@@ -48,6 +53,7 @@ int main(int argc, const char* argv[])
       exit(-1);
     }
     ftruncate(res_ds, 1 << i);
+    tm = clock();
     beg_res = (char*)mmap(NULL, 1 << i, PROT_WRITE | PROT_READ, MAP_SHARED, res_ds, 0);
     if (start == NULL)
     {
@@ -55,8 +61,23 @@ int main(int argc, const char* argv[])
       exit(-1);
     }
     memcpy(beg_res, start, 1 << i);
+    tm = (clock() - tm)*1.0/CLOCKS_PER_SEC;
+    log_out << "time_map " << tm << endl;
+
     munmap(start, 1 << i);
     munmap(beg_res, 1 << i);
+
+    ftruncate(res_ds, 0);
+    tm = clock();
+    for(;;)
+    {
+      len = read(exp[i - 10], buf, sizeof(buf));
+      if(len <= 0)
+        break;
+      write(res_ds, buf, len);
+    }
+    tm = (clock() - tm)*1.0/CLOCKS_PER_SEC;
+    log_out << "time_rw " << tm << endl;
   }
   for(int i = 10; i <= 25; ++i)
   {
